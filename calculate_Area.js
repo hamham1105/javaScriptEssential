df95582c6ad27d33c0f6f0cd879ebbeae05ebd49
hamham1105 <phamhuongtrang1105@gmail.com>
2024-05-13 16:33:11 +0700

    Modify form into Grocery Calculation Form with groceryTracker()

--- calculate_Area.js
+++ calculate_Area.js
@@ -9,3 +9,14 @@ function calculateArea(){
     // modify the text content within that HTML element
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+
+function groceryTracker(){
+    grocery1 = parseFloat(document.getElementById("grocery1").value) ;
+    grocery2 = parseFloat(document.getElementById("grocery2").value) ;
+    grocery3 = parseFloat(document.getElementById("grocery3").value) ;
+
+    let sumAmount = grocery1 + grocery2 + grocery3;
+
+    document.getElementById("result").innerText = `The total amount is: $${sumAmount}`
+}
